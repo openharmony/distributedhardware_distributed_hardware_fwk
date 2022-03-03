f3a327477ab7d597c8c906e34d64b81d443fe5a2
tangfan <1824525929@qq.com>
2022-03-03 21:14:45 +0800

    fix ut
    Signed-off-by: tangfan <1824525929@qq.com>

--- services/distributedhardwarefwkserviceimpl/test/unittest/common/componentloader/src/component_loader_test.cpp
+++ services/distributedhardwarefwkserviceimpl/test/unittest/common/componentloader/src/component_loader_test.cpp
@@ -23,132 +23,130 @@ using namespace testing::ext;
 
 namespace OHOS {
 namespace DistributedHardware {
+namespace {
+    std::map<DHType, CompHandler> g_compHandlerMap;
+}
+
 void ComponentLoaderTest::SetUpTestCase(void) {}
 
 void ComponentLoaderTest::TearDownTestCase(void) {}
 
-void ComponentLoaderTest::SetUp() {}
+void ComponentLoaderTest::SetUp()
+{
+    ComponentLoader::GetInstance().Init();
+    g_compHandlerMap = ComponentLoader::GetInstance().compHandlerMap_;
+}
 
-void ComponentLoaderTest::TearDown() {}
+void ComponentLoaderTest::TearDown()
+{
+    ComponentLoader::GetInstance().UnInit();
+    g_compHandlerMap.clear();
+}
 
 /**
  * @tc.name: component_loader_test_001
- * @tc.desc: Verify the Init function.
+ * @tc.desc: Verify the GetLocalDHVersion function.
  * @tc.type: FUNC
  * @tc.require: AR000GHSK3
  */
 HWTEST_F(ComponentLoaderTest, component_loader_test_001, TestSize.Level0)
 {
-    auto ret = ComponentLoader::GetInstance().Init();
+    DHVersion dhVersion;
+    auto ret = ComponentLoader::GetInstance().GetLocalDHVersion(dhVersion);
     EXPECT_EQ(DH_FWK_SUCCESS, ret);
-    EXPECT_TRUE(ComponentLoader::GetInstance().compHandlerMap_.size() != 0);
 }
 
 /**
  * @tc.name: component_loader_test_002
- * @tc.desc: Verify the GetLocalDHVersion function.
+ * @tc.desc: Verify the GetHardwareHandler function.
  * @tc.type: FUNC
  * @tc.require: AR000GHSK3
  */
 HWTEST_F(ComponentLoaderTest, component_loader_test_002, TestSize.Level0)
 {
-    DHVersion dhVersion;
-    auto ret = ComponentLoader::GetInstance().GetLocalDHVersion(dhVersion);
-    EXPECT_EQ(DH_FWK_SUCCESS, ret);
-    EXPECT_TRUE(!dhVersion.compVersions.empty());
-}
-
-/**
- * @tc.name: component_loader_test_003
- * @tc.desc: Verify the GetAllCompTypes function.
- * @tc.type: FUNC
- * @tc.require: AR000GHSK3
- */
-HWTEST_F(ComponentLoaderTest, component_loader_test_003, TestSize.Level0)
-{
-    std::vector<DHType> dhTypes = ComponentLoader::GetInstance().GetAllCompTypes();
-    EXPECT_TRUE(dhTypes.size() != 0);
+    for (const auto &iter : g_compHandlerMap) {
+        IHardwareHandler *hardwareHandlerPtr = nullptr;
+        auto ret = ComponentLoader::GetInstance().GetHardwareHandler(iter.first, hardwareHandlerPtr);
+        EXPECT_EQ(DH_FWK_SUCCESS, ret);
+        EXPECT_TRUE(hardwareHandlerPtr != nullptr);
+    }
 }
 
 /**
  * @tc.name: component_loader_test_004
- * @tc.desc: Verify the GetHardwareHandler function.
+ * @tc.desc: Verify the GetSource function.
  * @tc.type: FUNC
  * @tc.require: AR000GHSK3
  */
 HWTEST_F(ComponentLoaderTest, component_loader_test_004, TestSize.Level0)
 {
-    IHardwareHandler *hardwareHandlerPtr = nullptr;
-    auto ret = ComponentLoader::GetInstance().GetHardwareHandler(DHType::CAMERA, hardwareHandlerPtr);
-    EXPECT_EQ(DH_FWK_SUCCESS, ret);
-    EXPECT_TRUE(hardwareHandlerPtr != nullptr);
+    for (const auto &iter : g_compHandlerMap) {
+        IDistributedHardwareSource *sourcePtr = nullptr;
+        auto ret = ComponentLoader::GetInstance().GetSource(iter.first, sourcePtr);
+        EXPECT_EQ(DH_FWK_SUCCESS, ret);
+        EXPECT_TRUE(sourcePtr != nullptr);
+    }
 }
 
 /**
  * @tc.name: component_loader_test_005
- * @tc.desc: Verify the GetSource function.
+ * @tc.desc: Verify the GetSink function.
  * @tc.type: FUNC
  * @tc.require: AR000GHSK3
  */
 HWTEST_F(ComponentLoaderTest, component_loader_test_005, TestSize.Level0)
 {
-    IDistributedHardwareSource *sourcePtr = nullptr;
-    auto ret = ComponentLoader::GetInstance().GetSource(DHType::CAMERA, sourcePtr);
-    EXPECT_EQ(DH_FWK_SUCCESS, ret);
-    EXPECT_TRUE(sourcePtr != nullptr);
+    for (const auto &iter : g_compHandlerMap) {
+        IDistributedHardwareSink *sinkPtr = nullptr;
+        auto ret = ComponentLoader::GetInstance().GetSink(iter.first, sinkPtr);
+        EXPECT_EQ(DH_FWK_SUCCESS, ret);
+        EXPECT_TRUE(sinkPtr != nullptr);
+    }
 }
 
 /**
  * @tc.name: component_loader_test_006
- * @tc.desc: Verify the GetSink function.
+ * @tc.desc: Verify the ReleaseHardwareHandler function.
  * @tc.type: FUNC
  * @tc.require: AR000GHSK3
  */
 HWTEST_F(ComponentLoaderTest, component_loader_test_006, TestSize.Level0)
 {
-    IDistributedHardwareSink *sinkPtr = nullptr;
-    auto ret = ComponentLoader::GetInstance().GetSink(DHType::CAMERA, sinkPtr);
-    EXPECT_EQ(DH_FWK_SUCCESS, ret);
-    EXPECT_TRUE(sinkPtr != nullptr);
+    for (const auto &iter : g_compHandlerMap) {
+        auto ret = ComponentLoader::GetInstance().ReleaseHardwareHandler(iter.first);
+        EXPECT_EQ(DH_FWK_SUCCESS, ret);
+        EXPECT_TRUE(ComponentLoader::GetInstance().compHandlerMap_[iter.first].hardwareHandler == nullptr);
+    }
 }
 
 /**
  * @tc.name: component_loader_test_007
- * @tc.desc: Verify the ReleaseHardwareHandler function.
+ * @tc.desc: Verify the ReleaseSource function.
  * @tc.type: FUNC
  * @tc.require: AR000GHSK3
  */
 HWTEST_F(ComponentLoaderTest, component_loader_test_007, TestSize.Level0)
 {
-    auto ret = ComponentLoader::GetInstance().ReleaseHardwareHandler(DHType::CAMERA);
-    EXPECT_EQ(DH_FWK_SUCCESS, ret);
-    EXPECT_TRUE(ComponentLoader::GetInstance().compHandlerMap_[DHType::CAMERA].hardwareHandler == nullptr);
+    for (const auto &iter : g_compHandlerMap) {
+        auto ret = ComponentLoader::GetInstance().ReleaseSource(iter.first);
+        EXPECT_EQ(DH_FWK_SUCCESS, ret);
+        EXPECT_TRUE(ComponentLoader::GetInstance().compHandlerMap_[iter.first].sourceHandler == nullptr);
+    }
 }
 
 /**
  * @tc.name: component_loader_test_008
- * @tc.desc: Verify the ReleaseSource function.
+ * @tc.desc: Verify the ReleaseSink function.
  * @tc.type: FUNC
  * @tc.require: AR000GHSK3
  */
 HWTEST_F(ComponentLoaderTest, component_loader_test_008, TestSize.Level0)
 {
-    auto ret = ComponentLoader::GetInstance().ReleaseSource(DHType::CAMERA);
-    EXPECT_EQ(DH_FWK_SUCCESS, ret);
-    EXPECT_TRUE(ComponentLoader::GetInstance().compHandlerMap_[DHType::CAMERA].sourceHandler == nullptr);
-}
-
-/**
- * @tc.name: component_loader_test_009
- * @tc.desc: Verify the ReleaseSink function.
- * @tc.type: FUNC
- * @tc.require: AR000GHSK3
- */
-HWTEST_F(ComponentLoaderTest, component_loader_test_009, TestSize.Level0)
-{
-    auto ret = ComponentLoader::GetInstance().ReleaseSink(DHType::CAMERA);
-    EXPECT_EQ(DH_FWK_SUCCESS, ret);
-    EXPECT_TRUE(ComponentLoader::GetInstance().compHandlerMap_[DHType::CAMERA].sinkHandler == nullptr);
+    for (const auto &iter : g_compHandlerMap) {
+        auto ret = ComponentLoader::GetInstance().ReleaseSink(iter.first);
+        EXPECT_EQ(DH_FWK_SUCCESS, ret);
+        EXPECT_TRUE(ComponentLoader::GetInstance().compHandlerMap_[iter.first].sinkHandler == nullptr);
+    }
 }
 }
 }
