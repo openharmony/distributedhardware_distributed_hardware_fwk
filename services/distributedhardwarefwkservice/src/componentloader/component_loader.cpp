6575183a2c75340d62aa0b0487838c54afafe803
tangfan <tangfan5@huawei.com>
2022-09-28 19:30:29 +0800

    fix log style
    Signed-off-by: tangfan <tangfan5@huawei.com>

--- services/distributedhardwarefwkservice/src/componentloader/component_loader.cpp
+++ services/distributedhardwarefwkservice/src/componentloader/component_loader.cpp
@@ -214,7 +214,7 @@ void ComponentLoader::GetAllHandler(std::map<DHType, CompConfig> &dhtypeMap)
 int32_t ComponentLoader::GetHardwareHandler(const DHType dhType, IHardwareHandler *&hardwareHandlerPtr)
 {
     if (compHandlerMap_.find(dhType) == compHandlerMap_.end()) {
-        DHLOGE("DHType not exist, dhType: " PRIu32, (uint32_t)dhType);
+        DHLOGE("DHType not exist, dhType: %" PRIu32, (uint32_t)dhType);
         return ERR_DH_FWK_LOADER_HANDLER_IS_NULL;
     }
 
@@ -238,7 +238,7 @@ int32_t ComponentLoader::GetHardwareHandler(const DHType dhType, IHardwareHandle
 int32_t ComponentLoader::GetSource(const DHType dhType, IDistributedHardwareSource *&sourcePtr)
 {
     if (compHandlerMap_.find(dhType) == compHandlerMap_.end()) {
-        DHLOGE("DHType not exist, dhType: " PRIu32, (uint32_t)dhType);
+        DHLOGE("DHType not exist, dhType: %" PRIu32, (uint32_t)dhType);
         return ERR_DH_FWK_LOADER_HANDLER_IS_NULL;
     }
 
@@ -262,7 +262,7 @@ int32_t ComponentLoader::GetSource(const DHType dhType, IDistributedHardwareSour
 int32_t ComponentLoader::GetSink(const DHType dhType, IDistributedHardwareSink *&sinkPtr)
 {
     if (compHandlerMap_.find(dhType) == compHandlerMap_.end()) {
-        DHLOGE("DHType not exist, dhType: " PRIu32, (uint32_t)dhType);
+        DHLOGE("DHType not exist, dhType: %" PRIu32, (uint32_t)dhType);
         return ERR_DH_FWK_LOADER_HANDLER_IS_NULL;
     }
 
@@ -404,7 +404,7 @@ bool ComponentLoader::IsDHTypeExist(DHType dhType)
 int32_t ComponentLoader::GetSourceSaId(const DHType dhType)
 {
     if (compHandlerMap_.find(dhType) == compHandlerMap_.end()) {
-        DHLOGE("DHType not exist, dhType: " PRIu32, (uint32_t)dhType);
+        DHLOGE("DHType not exist, dhType: %" PRIu32, (uint32_t)dhType);
         return DEFAULT_SA_ID;
     }
     return compHandlerMap_[dhType].sourceSaId;
