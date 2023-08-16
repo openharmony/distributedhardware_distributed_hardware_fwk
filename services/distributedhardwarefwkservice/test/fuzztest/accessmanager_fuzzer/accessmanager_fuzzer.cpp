6f5363ff7d3d94bfc24d4a203f5a2886e75e72e1
wuxiaodong02 <wuxiaodong21@huawei.com>
2023-08-16 18:04:49 +0800

    modify fuzz
    Signed-off-by: wuxiaodong02 <wuxiaodong21@huawei.com>

--- services/distributedhardwarefwkservice/test/fuzztest/accessmanager_fuzzer/accessmanager_fuzzer.cpp
+++ services/distributedhardwarefwkservice/test/fuzztest/accessmanager_fuzzer/accessmanager_fuzzer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -35,7 +35,7 @@ namespace {
 
 void AccessManagerFuzzTest(const uint8_t* data, size_t size)
 {
-    if ((data == nullptr) || (size <= sizeof(DmDeviceInfo))) {
+    if ((data == nullptr) || (size > DM_MAX_DEVICE_ID_LEN)) {
         return;
     }
 
