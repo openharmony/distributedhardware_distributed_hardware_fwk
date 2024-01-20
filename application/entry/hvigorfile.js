50f5fe5577a937c30f03bf92462d7c241d1447fb
chen0088 <chenmenghuan6@huawei.com>
2024-01-20 16:44:54 +0800

    modify codecheck
    Signed-off-by: chen0088 <chenmenghuan6@huawei.com>

--- application/entry/hvigorfile.js
+++ application/entry/hvigorfile.js
@@ -12,9 +12,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
 
 export default {
-    system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
-}
+  system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins:[] /* Custom plugin to extend the functionality of Hvigor. */
+};
