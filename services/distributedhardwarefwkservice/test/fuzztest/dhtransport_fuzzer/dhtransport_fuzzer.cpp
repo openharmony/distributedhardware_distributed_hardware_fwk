61b9b5decfdc56b0b7f036e9d55a4f10903c2620
li-tiangang4 <litiangang4@huawei.com>
2024-07-19 16:42:59 +0800

    add fuzz
    Signed-off-by: li-tiangang4 <litiangang4@huawei.com>

--- services/distributedhardwarefwkservice/test/fuzztest/dhtransport_fuzzer/dhtransport_fuzzer.cpp
+++ services/distributedhardwarefwkservice/test/fuzztest/dhtransport_fuzzer/dhtransport_fuzzer.cpp
@@ -162,6 +162,38 @@ void DhTransportParseAndSaveRemoteDHCapsFuzzTest(const uint8_t* data, size_t siz
     std::shared_ptr<DHCommTool> dhCommTool = std::make_shared<DHCommTool>();
     dhCommTool->ParseAndSaveRemoteDHCaps(remoteCaps);
 }
+
+void DhTransportOnSocketOpenedFuzzTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size < sizeof(int32_t))) {
+        return;
+    }
+    int32_t socketId = *(reinterpret_cast<const int32_t*>(data));
+    std::string peerSocketName(reinterpret_cast<const char*>(data), size);
+    std::string remoteNetworkId(reinterpret_cast<const char*>(data), size);
+    PeerSocketInfo info = {
+        .name = const_cast<char*>(peerSocketName.c_str()),
+        .networkId = const_cast<char*>(remoteNetworkId.c_str()),
+        .pkgName = const_cast<char*>(DH_FWK_PKG_NAME.c_str()),
+        .dataType = DATA_TYPE_BYTES
+    };
+    std::shared_ptr<DHCommTool> dhCommTool = std::make_shared<DHCommTool>();
+    std::shared_ptr<DHTransport> dhTransportTest = std::make_shared<DHTransport>(dhCommTool);
+    dhTransportTest->OnSocketOpened(socketId, info);
+}
+
+void DhTransportOnSocketClosedFuzzTest(const uint8_t* data, size_t size)
+{
+    if ((data == nullptr) || (size < sizeof(int32_t))) {
+        return;
+    }
+    int32_t socketId = *(reinterpret_cast<const int32_t*>(data));
+    ShutdownReason reason = ShutdownReason::SHUTDOWN_REASON_UNKNOWN;
+    std::shared_ptr<DHCommTool> dhCommTool = std::make_shared<DHCommTool>();
+    std::shared_ptr<DHTransport> dhTransportTest = std::make_shared<DHTransport>(dhCommTool);
+    dhTransportTest->OnSocketClosed(socketId, reason);
+    dhTransportTest->UnInit();
+}
 }
 }
 
@@ -174,11 +206,14 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
     OHOS::DistributedHardware::DhTransportCreateClientSocketFuzzTest(data, size);
     OHOS::DistributedHardware::DhTransportIsDeviceSessionOpenedFuzzTest(data, size);
     OHOS::DistributedHardware::DhTransportClearDeviceSocketOpenedFuzzTest(data, size);
+    OHOS::DistributedHardware::DhTransportStartSocketFuzzTest(data, size);
     OHOS::DistributedHardware::DhTransportStopSocketFuzzTest(data, size);
     OHOS::DistributedHardware::DhTransportSendFuzzTest(data, size);
     OHOS::DistributedHardware::DhTransportTriggerReqFullDHCapsFuzzTest(data, size);
     OHOS::DistributedHardware::DhTransportGetAndSendLocalFullCapsFuzzTest(data, size);
     OHOS::DistributedHardware::DhTransportParseAndSaveRemoteDHCapsFuzzTest(data, size);
+    OHOS::DistributedHardware::DhTransportOnSocketOpenedFuzzTest(data, size);
+    OHOS::DistributedHardware::DhTransportOnSocketClosedFuzzTest(data, size);
     return 0;
 }
 
