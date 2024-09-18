60b29678786faaa0c411b3d08f8dd3a1c6fd2ce2
li-tiangang4 <litiangang4@huawei.com>
2024-09-18 10:50:54 +0800

    多设备上下线时序问题回合5.0
    Signed-off-by: li-tiangang4 <litiangang4@huawei.com>

--- services/distributedhardwarefwkservice/src/utils/dh_context.cpp
+++ services/distributedhardwarefwkservice/src/utils/dh_context.cpp
@@ -135,11 +135,11 @@ void DHContext::AddOnlineDevice(const std::string &udid, const std::string &uuid
     devIdEntrySet_.insert(idEntry);
 }
 
-void DHContext::RemoveOnlineDeviceByUUID(const std::string &uuid)
+void DHContext::RemoveOnlineDeviceIdEntryByNetworkId(const std::string &networkId)
 {
     std::unique_lock<std::shared_mutex> lock(onlineDevMutex_);
     for (auto iter = devIdEntrySet_.begin(); iter != devIdEntrySet_.end(); iter++) {
-        if (iter->uuid == uuid) {
+        if (iter->networkId == networkId) {
             devIdEntrySet_.erase(iter);
             break;
         }
@@ -273,6 +273,26 @@ std::string DHContext::GetDeviceIdByDBGetPrefix(const std::string &prefix)
     }
 
     return id;
+}
+
+void DHContext::AddRealTimeOnlineDeviceNetworkId(const std::string &networkId)
+{
+    DHLOGI("AddRealTimeOnlineDeviceNetworkId: %{public}s", GetAnonyString(networkId).c_str());
+    std::shared_lock<std::shared_mutex> lock(realTimeNetworkIdMutex_);
+    realTimeOnLineNetworkIdSet_.insert(networkId);
+}
+
+void DHContext::DeleteRealTimeOnlineDeviceNetworkId(const std::string &networkId)
+{
+    DHLOGI("DeleteRealTimeOnlineDeviceNetworkId: %{public}s", GetAnonyString(networkId).c_str());
+    std::shared_lock<std::shared_mutex> lock(realTimeNetworkIdMutex_);
+    realTimeOnLineNetworkIdSet_.erase(networkId);
+}
+
+size_t DHContext::GetRealTimeOnlineDeviceCount()
+{
+    std::shared_lock<std::shared_mutex> lock(realTimeNetworkIdMutex_);
+    return realTimeOnLineNetworkIdSet_.size();
 }
 
 void DHContext::RegisDHFWKIsomerismListener()
