aa520dc74558c706bc5adc772e00276cea132517
li-tiangang4 <litiangang4@huawei.com>
2025-02-10 23:07:27 +0800

    低概率时序问题，dhfwk多次注册通知消息偶现投音失败
    Signed-off-by: li-tiangang4 <litiangang4@huawei.com>

--- services/distributedhardwarefwkservice/src/publisher/publisher_item.cpp
+++ services/distributedhardwarefwkservice/src/publisher/publisher_item.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -71,6 +71,7 @@ void PublisherItem::PublishMessage(const std::string &message)
     }
     std::lock_guard<std::mutex> lock(mutex_);
     for (const auto &listener : listeners_) {
+        DHLOGI("Publish Message topic: %{public}d", topic_);
         listener->OnMessage(topic_, message);
     }
 }
