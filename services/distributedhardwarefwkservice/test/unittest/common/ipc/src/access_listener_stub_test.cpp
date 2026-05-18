18843ebd31e7b9ae1baa713ca4db82fcf6d883c5
wei-guoqing1 <weiguoqing2@h-partners.com>
2026-05-18 15:09:38 +0800

    test: add access_listener_stub unit test
    Add tests for IPC stub: on_remote_request_invalid_token
    
    Co-Authored-By: Agent
    Signed-off-by: wei-guoqing1 <weiguoqing2@h-partners.com>
    Change-Id: Ifbfff6cc1e2936b0dbb86c5c369227ebb327eed2

---
+++ services/distributedhardwarefwkservice/test/unittest/common/ipc/src/access_listener_stub_test.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "access_listener_stub.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using namespace testing::ext;
+using namespace OHOS::DistributedHardware;
+
+class MockAccessListenerStub : public AccessListenerStub {
+public:
+    MOCK_METHOD4(OnRequestHardwareAccess, void(const std::string&, AuthDeviceInfo, DHType, const std::string&));
+};
+
+class AccessListenerStubTest : public testing::Test {
+protected:
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+HWTEST_F(AccessListenerStubTest, test_on_remote_request_invalid_token, TestSize.Level1)
+{
+    auto stub = std::make_shared<AccessListenerStub>();
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    data.WriteInterfaceToken(u"invalid_token");
+    int32_t ret = stub->OnRemoteRequest(0, data, reply, option);
+    EXPECT_EQ(ret, ERR_INVALID_DATA);
+}
